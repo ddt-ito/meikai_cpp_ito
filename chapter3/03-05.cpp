25e582759ef07eb67b07207d7639c641c86bbfdf
ddt-ito <ddt.ito@gmail.com>
2018-10-12 10:47:25 +0900

    指摘事項の修正2回目

--- chapter3/03-05.cpp
+++ chapter3/03-05.cpp
@@ -11,29 +11,30 @@ using namespace std;
 
 // メイン関数（以下の文章を実行。）
 int main() {
-	// 整数を格納するために変数xを宣言
-	int integerNumberX;
+
+	// 出力するアスタリスクの個数を格納するために変数xを宣言
+	int asteriskNumber;
 
 	// 文章の出力
 	cout << "何個*を表示しますか : ";
 
-	// 受け取った数をxに代入
-	cin >> integerNumberX;
+	// 受け取った数を変数xに代入
+	cin >> asteriskNumber;
 
-	// xの値が1以上の場合にアスタリスクを出力
-	if (integerNumberX >= 1) {
+	// xの値が1以上の場合にのみアスタリスク出力後に改行文字を出力
+	if (asteriskNumber >= 1) {
 
-		// 整数を格納するための変数yを宣言、0で初期化
-		int integerNumberY = 0;
+		// 以下while文のループ回数を格納するために変数yを宣言、0で初期化
+		int countNumber = 0;
 
-		// yの値がxの値よりも小さい間、{}内を実行する
-		while (integerNumberY < integerNumberX) {
+		// yの値がxの値よりも小さい間、アスタリスクをひとつずつ出力する
+		while (countNumber < asteriskNumber) {
 
 			// アスタリスクの出力
 			cout << '*';
 
 			// yの値をインクリメント
-			integerNumberY++;
+			countNumber++;
 		}
 		// 改行文字の出力
 		cout << '\n';
