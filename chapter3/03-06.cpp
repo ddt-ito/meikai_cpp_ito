8c952cd4893b789ca060e860ebf2daf53e804719
ddt-ito <ddt.ito@gmail.com>
2018-10-12 10:37:51 +0900

    指摘事項の修正2回目

--- chapter3/03-06.cpp
+++ chapter3/03-06.cpp
@@ -11,35 +11,36 @@ using namespace std;
 
 // メイン関数（以下の文章を実行。）
 int main() {
-	// 整数を格納するために変数xを宣言
-	int integerNumberX;
+
+	// 出力する記号の個数を格納するために変数xを宣言
+	int symbolNumber;
 
 	// 文章の出力
-	cout << "何個*を表示しますか : ";
+	cout << "何個表示しますか : ";
 
 	// 入力から受け取った数をxに代入
-	cin >> integerNumberX;
+	cin >> symbolNumber;
 
-	// 整数を格納するために変数yを宣言、0で初期化
-	int integerNumberY = 0;
+	// 以下while文のループ回数を格納するために変数yを宣言、0で初期化
+	int countNumber = 0;
 
-	// y < xである間、記号を出力する
-	while (integerNumberY < integerNumberX) {
+	// y < xである間、記号をひとつずつ出力する
+	while (countNumber < symbolNumber) {
 
-		// xの値が偶数の場合には'+'を出力
-		if (integerNumberY % 2 == 0) {
+		// xの値が偶数（2で割ったときの剰余が0）の場合には'+'を出力
+		if (countNumber % 2 == 0) {
 
 			// '+'の出力
 			cout << '+';
 		}
-		// xの値が奇数の場合には'-'を出力する
-		else if (integerNumberY % 2 == 1) {
+		// xの値が奇数（2で割ったときの剰余が1）の場合には'-'を出力する
+		else if (countNumber % 2 == 1) {
 
 			// '-'の出力
 			cout << '-';
 		}
 		// yの値をインクリメント
-		integerNumberY++;
+		countNumber++;
 	}
 	// 改行文字の出力
 	cout << '\n';
