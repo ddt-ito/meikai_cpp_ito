b7ed2ad5e51f6993da75d4c7f53bd2429cc63d3b
ddt-ito <ddt.ito@gmail.com>
2018-10-12 09:56:40 +0900

    指摘事項の修正2回目

--- chapter3/03-04.cpp
+++ chapter3/03-04.cpp
@@ -11,7 +11,8 @@ using namespace std;
 
 // メイン関数（以下の文章を実行。）
 int main() {
-	// 入力値を格納するための変数を宣言
+
+	// 入力値を格納するための変数xを宣言
 	int integerNumber;
 
 	// 文章の出力
@@ -19,24 +20,24 @@ int main() {
 
 	// while()内の条件を満たしている間、{}内を1回以上実行
 	do {
-		// 整数値を出力
+		// xの値を出力
 		cout << "正の整数値 : ";
 
-		// 入力から受け取った数を整数値に代入
+		// 入力から受け取った数を変数xに代入
 		cin >> integerNumber;
 
-	// do{}を繰り返す条件（整数値 <= 0である間）
+	// do{}を繰り返す条件（x <= 0である間）
 	} while (integerNumber <= 0);
 
-	// 整数値 >= 0である間、{}内を実行
+	// 入力値 >= 0である間、{}内を実行
 	while (integerNumber >= 0) {
 
-		// 整数値の出力
+		// xの値を出力
 		cout << integerNumber << "\n";
 
-		// 整数値を1減らす
+		// xの値をデクリメント
 		integerNumber--;
 	}
-	// while文終了時の整数値が-1であることの確認
+	// while文終了時、xの値が-1であることの確認
 	cout << "カウントダウン終了時の整数値 : " << integerNumber;
 }
