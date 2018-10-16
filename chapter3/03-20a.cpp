32b825dc6297aeb5987895f336929b13a83a902a
ddt-ito <ddt.ito@gmail.com>
2018-10-16 14:54:04 +0900

    add text, 03-20a 初回アップロード

---
+++ chapter3/03-20a.cpp
@@ -0,0 +1,42 @@
+﻿/*
+演習3-20 List3-14は左下側が直角三角形を表示するプログラムであった。
+直角が左上側、右下側、右上側の直角三角形を表示するプログラムをそれぞれ作成せよ。
+（それぞれファイル名を「03-20a」（左上側）、「03-20b」（右下側）、「03-20c」（右上側）とする。
+*/
+
+// 03-20a
+
+// 入出力のためのインクルード
+#include <iostream>
+
+// 名前空間の利用
+using namespace std;
+
+// メイン関数（以下を実行）
+int main() {
+
+	// 一辺の長さの数値を格納するための変数を宣言
+	int areaNumber;
+
+	// 文章を出力
+	cout << "左上直角の三角形を表示します。\n";
+
+	// 文章を出力
+	cout << "段数は : ";
+
+	// 入力から受け取った数値を変数に代入
+	cin >> areaNumber;
+
+	// 三角形をつくるための記号の出力、縦方向にループ
+	for (int lineNumber = 0; lineNumber <= areaNumber - 1; lineNumber++) {
+
+		// 三角形をつくるための記号の出力、横方向にループ
+		for (int columnNumber = areaNumber; columnNumber - lineNumber >= 1; columnNumber--) {
+
+			// 記号を出力
+			cout << '*';
+		}
+		// 改行文字を出力（lineNumberの値の数だけ記号を横方向に出力した時）
+		cout << '\n';
+	}
+}
