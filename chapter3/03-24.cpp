eb9ae14a9253918fa95770046d686f0eee847908
ddt-ito <ddt.ito@gmail.com>
2018-10-18 17:30:13 +0900

    add text, 03-24, 初回アップロード

---
+++ chapter3/03-24.cpp
@@ -0,0 +1,64 @@
+﻿/*
+演習3-24 合計だけでなく平均も求めるように、List3-17を書き換えたプログラムを作成せよ。
+読み込んだ負の数の個数は平均を求める際の分母から除外すること。
+*/
+
+// 入出力のためのインクルード
+#include <iostream>
+
+// 名前空間の利用
+using namespace std;
+
+// メイン関数（以下を実行）
+int main() {
+
+	// 整数を加算する回数を格納するための変数を宣言
+	int numberOfTimes;
+
+	// 文章の出力
+	cout << "整数を加算します。\n";
+
+	// 文章の出力
+	cout << "何個加算しますか : ";
+
+	// 入力から受け取った数を変数に代入
+	cin >> numberOfTimes;
+
+	// 整数の合計値を格納するための変数を宣言、0で初期化
+	int sumNumber = 0;
+
+	// 現在の加算回数を格納するための変数を宣言、初期化
+	// はじめに入力した整数の加算回数だけ加算を繰り返す
+	for (int countNumber = 1; countNumber <= numberOfTimes; ) {
+
+		// 加算する整数を格納するための変数を宣言
+		int integerNumber;
+
+		// 文章の出力
+		cout << "整数 : ";
+
+		// 入力から受け取った数を変数に代入
+		cin >> integerNumber;
+
+		// 負の数の入力をした場合
+		// 加算は行わず回数のインクリメントもしない
+		if (integerNumber < 0){
+
+			// 文章の出力
+			cout << "\a負の数は加算しません。\n";
+
+			// これより後の文の実行をスキップ
+			continue;
+		} 
+		// 入力した整数値を現時点の合計値に加算
+		sumNumber += integerNumber;
+
+		// 加算回数をインクリメント
+		countNumber++;
+	}
+	// 合計値の出力
+	cout << "合計は" << sumNumber << "です。\n";
+
+	// 平均値の出力
+	cout << "平均は" << sumNumber / numberOfTimes << "です。";
+}
